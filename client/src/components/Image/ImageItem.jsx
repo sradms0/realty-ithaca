e82fe4fd3de0696d01f4930fa7e3ce854ccaf16f
sradms0 <sradms0@openmailbox.org>
2019-04-05 16:14:59 -0400

    add Modal view for image

--- client/src/components/Image/ImageItem.jsx
+++ client/src/components/Image/ImageItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Card, Image, Item } from 'semantic-ui-react'
+import { Button, Card, Image, Item, Modal } from 'semantic-ui-react'
 
 export default function ImageItem({ src, upload, edit=null }) {
   // return the preview of an pre-uploaded image and enable removal
@@ -28,14 +28,22 @@ export default function ImageItem({ src, upload, edit=null }) {
       <Image src={src}/>
       <Card.Content extra>
         <div className='ui two buttons'>
-          <Button 
-            compact
-            icon='window maximize'
-            color='teal'
-            basic
-            className='view'
-            onClick={() => console.log(src)}
-          />
+          {/* show large view of image */}
+          <Modal 
+            trigger={
+              <Button 
+                compact
+                icon='window maximize'
+                color='teal'
+                basic
+                className='view'
+              />
+            } 
+          >
+            <Modal.Content image>
+              <Image size='large' src={src} centered />
+            </Modal.Content>
+          </Modal>
           <Button 
             compact
             icon='delete'
