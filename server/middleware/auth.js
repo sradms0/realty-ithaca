de0416c17a3e47fa06c036af17725416fa31b831
sradms0 <sradms0@openmailbox.org>
2019-05-01 17:26:32 -0400

    add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const jwt       = require('jsonwebtoken');
+const dotenv    = require('dotenv').config({ path: '../' });
+
+exports.auth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+
+  // check for a token
+  if (!token) {
+    const err = new Error('Access denied');
+    err.status = 401;
+    return next(err);
+  }
+
+  try {
+    // verify token
+    const decoded = jwt.verify(token, process.env.SECRET)
+    // add user from payload
+    req.user = decoded;
+    return next();
+  } catch (err) {
+    err.status = 400;
+    return next(err);
+  }
+}
