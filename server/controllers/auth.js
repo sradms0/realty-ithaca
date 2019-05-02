d4f96b9206060daaf99a15144d4cad714fbce2ed
sradms0 <sradms0@openmailbox.org>
2019-05-01 21:08:17 -0400

    assign jwt to res.cookie and add user controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,16 @@
 'user strict';
 const User = require('../models/user');
+const { asyncHandler } = require('./util/err');
 
 exports.login = (req, res, next) => {
   const { email, password } = req.body;
   if (email && password) {
     User.authenticate(email, password, (err, user) => {
       if (err || !user) return next(err);
-      return res.json(user);
+      const { token } = user;
+      // assign jwt to cookie
+      res.cookie('jwt', token, { httpOnly: true, secure: true });
+      return res.json({ email });
     });
   } else {
     const err = new Error('Email and password required');
@@ -14,3 +18,11 @@ exports.login = (req, res, next) => {
     return next(err);
   }
 };
+
+exports.user = asyncHandler(async (req, res, next) => {
+  console.log(req.user);
+  const user = await User.findOne({ _id: req.user._id })
+    .select('-password');
+  console.log(user);
+  return res.json(user);
+})
