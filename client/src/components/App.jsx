909baa15f4795ca354c5a187a32fa20f2f6bce34
sradms0 <sradms0@openmailbox.org>
2019-05-04 17:56:53 -0400

    import PrivateRoutes component and move routes to its config

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // nested routing components
 import ImageRoutes from './Image/ImageRoutes';
 import AddressRoutes from './Address/AddressRoutes';
 import ListingRoutes from './Listing/ListingRoutes';
+import PrivateRoutes from './Private/PrivateRoutes';
 
 import Login from './Login/Login';
 import NavigationMenu from './Header/NavigationMenu';
 
 export default class App extends Component {
   state = {
-    message: ''
+    message: '',
+    authd: false
   }
 
   componentDidMount() {
@@ -24,16 +26,30 @@ export default class App extends Component {
     this.setState({ message: res.data.message });
   }
 
+  authorize = () => {
+    this.setState({ authd: true });
+  }
+
   render() {
+    const { authd } = this.state;
     return (
       <BrowserRouter>
         <div className="App">
-          <Route path='/login' component={Login} />
+          <Switch>
+            <Route 
+              path='/login' 
+              render={() => (<Login authorize={this.authorize} />)} 
+            />
 
-          <Route component={NavigationMenu}/>
-          <Route path='/image' component={ImageRoutes} />
-          <Route path='/address' component={AddressRoutes} />
-          <Route path='/listing' component={ListingRoutes} />
+            <PrivateRoutes 
+              authd={authd} 
+              config={[
+                  {path: '/image', component: ImageRoutes}, 
+                  {path: '/address', component: AddressRoutes}, 
+                  {path: '/listing', component: ListingRoutes}
+                ]} 
+            />
+          </Switch>
         </div>
       </BrowserRouter>
     );
