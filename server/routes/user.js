4c5d56c06d636cbc9639ba01275cc2e21fca8518
sradms0 <sradms0@openmailbox.org>
2019-05-05 19:59:06 -0400

    remove previous "auth" get route and add "user","login","logout" post routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,13 @@ const controller = require('../controllers/user');
 const { auth } = require('../middleware/auth');
 
 module.exports = app => {
-  app.route('/api/auth')
-    .get(auth, controller.user)
+  app.route('/api/user')
+    .post(auth, controller.user)
+
+  app.route('/api/user/login')
     .post(controller.login);
+
+  app.route('/api/user/logout')
+    .post(controller.logout);
 }
 
