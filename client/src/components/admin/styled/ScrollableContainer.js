4b9c3e1790be770327c460d371d550caca5b11f3
sradms0 <sradms0@openmailbox.org>
2019-05-19 10:52:37 -0400

    add styled scrollable container

---
+++ client/src/components/admin/styled/ScrollableContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Container } from 'semantic-ui-react';
+
+export default styled(Container)`
+  &&& {
+    height: 250px;
+    overflow: scroll;
+  }
+`;
